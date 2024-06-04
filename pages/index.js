addde9e616fcc24589b02b04fbe365ea3715ba1e
aliraza114 <aliraza3569@gmail.com>
2024-06-04 23:56:44 +0500

    add data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,62 @@
+import MeetupList from '@/components/meetups/MeetupList';
+
+const MEETUP = [
+    {
+        id: 'm1',
+        title: 'My First Meetup',
+        image: 'https://tse3.mm.bing.net/th?id=OIP.ESoS0_MqVlu3zmvK67lqKQHaE7&pid=Api&P=0&h=220',
+        address: 'chink pokli 234 555, 5',
+        description: 'Jani gaga ',
+    },
+    {
+        id: 'm2',
+        title: 'My 33 Meetup',
+        image: 'https://tse3.mm.bing.net/th?id=OIP.ESoS0_MqVlu3zmvK67lqKQHaE7&pid=Api&P=0&h=220',
+        address: 'chink pokli 234 555, 5',
+        description: 'Jani 33 gaga ',
+    },
+    
+    {
+        id: 'm3',
+        title: 'My 2 Meetup',
+        image: 'https://tse3.mm.bing.net/th?id=OIP.ESoS0_MqVlu3zmvK67lqKQHaE7&pid=Api&P=0&h=220',
+        address: 'chink pokli 234 555, 5',
+        description: 'Jani 2 gaga ',
+    },
+    {
+        id: 'm4',
+        title: 'My First Meetup',
+        image: 'https://tse3.mm.bing.net/th?id=OIP.ESoS0_MqVlu3zmvK67lqKQHaE7&pid=Api&P=0&h=220',
+        address: 'chink pokli 234 555, 5',
+        description: 'Jani gaga ',
+    },
+    
+    {
+        id: 'm1',
+        title: 'My foue Meetup',
+        image: 'https://tse3.mm.bing.net/th?id=OIP.ESoS0_MqVlu3zmvK67lqKQHaE7&pid=Api&P=0&h=220',
+        address: 'chink pokli 234 555, 5',
+        description: 'Jani foue gaga ',
+    },
+    {
+        id: 'm5',
+        title: 'My five Meetup',
+        image: 'https://tse3.mm.bing.net/th?id=OIP.ESoS0_MqVlu3zmvK67lqKQHaE7&pid=Api&P=0&h=220',
+        address: 'chink pokli 234 555, 5',
+        description: 'Jani five gaga ',
+    },
+    {
+        id: 'm6',
+        title: 'My Six Meetup',
+        image: 'https://tse3.mm.bing.net/th?id=OIP.ESoS0_MqVlu3zmvK67lqKQHaE7&pid=Api&P=0&h=220',
+        address: 'chink pokli 234 555, 5',
+        description: 'Jani six gaga ',
+    },
+]
+
 function HomePage() {
     return (
-        <h1>
-            Home Page
-        </h1>
+        <MeetupList meetups={MEETUP} />
     )
 }
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,25 +1,12 @@
+import MeetupList from '@/components/meetups/MeetupList';
 import Link from 'next/link';
 
-function NewsPage() {
+function NewMeetup() {
     return (
         <>
-        <h1>
-            Meetup Page
-        </h1>
-        <ul>
-            <li>
-                <Link href={'/news/next'}>
-                    Next JS
-                </Link>
-            </li>
-            <li>
-                <Link href={'/news/react'}>
-                    React JS
-                </Link>
-            </li>
-        </ul>
+        <h1> New Meetup </h1>
         </>
     )
 }
 
-export default NewsPage;
+export default NewMeetup;
